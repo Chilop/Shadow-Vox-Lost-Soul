c179925cf07e55f43cd6a8cfd9c4f5a0fd6f7c8c
Chilo <zap520@amerike.edu.mx>
2025-03-20 19:03:11 -0600

    Player controller updated

--- Game Shadow Vox Lost Soul/Assets/-Scripts/Player/PlayerController.cs
+++ Game Shadow Vox Lost Soul/Assets/-Scripts/Player/PlayerController.cs
@@ -4,45 +4,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-     private Rigidbody _rb; 
-     
+     [SerializeField] private Rigidbody _rb; 
      private float _movementX;
      private float _movementY;
      private float _movementZ;
      
-     private float speed = 2; 
-     private float jumpForce = 5f;
+     [SerializeField] private float speed = 3f; 
+     [SerializeField] private float jumpForce = 5f;
+     
+     [SerializeField] private Vector3 moveDirection;
      
      void Start()
         {
             _rb = GetComponent<Rigidbody>();
         }
      
-     void OnMove(InputValue movementValue)
+     public void OnMove(InputAction.CallbackContext context)
         {
-     // Convert the input value into a Vector2 for movement.
-            Vector2 movementVector = movementValue.Get<Vector2>();
+     
+            Vector2 movementVector = context.ReadValue<Vector2>();
     
-     // Store the X and Y components of the movement.
+     
             _movementX = movementVector.x; 
             _movementY = movementVector.y; 
+            
+            Debug.Log(context.ReadValue<Vector2>());
         }
     
-     // FixedUpdate is called once per fixed frame-rate frame.
-     private void FixedUpdate() 
+     
+     private void FixedUpdate()
         {
-     // Create a 3D movement vector using the X and Y inputs.
-            Vector3 movement = new Vector3 (_movementX, 0.0f, _movementY);
-    
-     // Apply force to the Rigidbody to move the player.
-            _rb.AddForce(movement * speed); 
+         moveDirection = new Vector3(_movementX,0, _movementY);
+            _rb.AddForce(moveDirection * speed);
         }
 
-        void Onjump(InputAction.CallbackContext ctx)
+        public void OnJump(InputAction.CallbackContext ctx)
         {
-            if (ctx.performed)
-            {
                 _rb.AddForce(Vector3.up * jumpForce);
-            }
+                Debug.Log(ctx.performed);
         }
 }
