cfc9292de18751a734687173ce8501cfaf3ddbf1
richdev2021 <algo7332@gmail.com>
2025-05-08 18:33:45 -0600

    menu config and changes

--- Game Shadow Vox Lost Soul/Assets/-Scripts/UI/MainMenu/MainMenuController.cs
+++ Game Shadow Vox Lost Soul/Assets/-Scripts/UI/MainMenu/MainMenuController.cs
@@ -1,9 +1,18 @@
+using System.Threading;
 using GameShadow;
 using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject G_MainMenu, G_SettingsMenu;
+    public bool G_ChangeMenuAnimTrigger;
+    public float G_Timer;
+    public int G_Inversor=-1;
 
+    public void Start()
+    {
+        G_Inversor = 1;
+    }
     public void StartGame(string SceneName)
     {
         SceneTransitionManager.Source.TransitionToScene(SceneName);
@@ -13,5 +22,24 @@ public class MainMenuController : MonoBehaviour
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void settingsToogle() {
+        if (!G_ChangeMenuAnimTrigger) { 
+            G_ChangeMenuAnimTrigger = true;
+            G_Inversor *= -1;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (G_ChangeMenuAnimTrigger) {
+            G_Timer += 1 * Time.fixedDeltaTime;
+            G_MainMenu.transform.position += new Vector3((G_Inversor*1445) * Time.fixedDeltaTime, 0, 0);
+            G_SettingsMenu.transform.position += new Vector3((G_Inversor*1445) * Time.fixedDeltaTime, 0, 0);
+            if (G_Timer >= 0.5f) {
+                G_Timer = 0;
+                G_ChangeMenuAnimTrigger= false;
+            }
+        }
     }
 }
