8799e9f1fd3f05fbf3199a5ceb37001a3d63b682
richdev2021 <94477023+richdev2021@users.noreply.github.com>
2025-05-13 18:00:07 -0600

    menu changes

--- Game Shadow Vox Lost Soul/Assets/-Scripts/UI/MainMenu/MainMenuController.cs
+++ Game Shadow Vox Lost Soul/Assets/-Scripts/UI/MainMenu/MainMenuController.cs
@@ -6,12 +6,12 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject G_MainMenu, G_SettingsMenu;
     public bool G_ChangeMenuAnimTrigger;
-    public float G_Timer;
+    public float G_Timer, G_distance;
     public int G_Inversor=-1;
 
     public void Start()
     {
-        G_Inversor = 1;
+        G_Inversor = -1;
     }
     public void StartGame(string SceneName)
     {
@@ -34,8 +34,8 @@ public class MainMenuController : MonoBehaviour
     {
         if (G_ChangeMenuAnimTrigger) {
             G_Timer += 1 * Time.fixedDeltaTime;
-            G_MainMenu.transform.position += new Vector3((G_Inversor*1445) * Time.fixedDeltaTime, 0, 0);
-            G_SettingsMenu.transform.position += new Vector3((G_Inversor*1445) * Time.fixedDeltaTime, 0, 0);
+            G_MainMenu.transform.position += new Vector3((G_Inversor*G_distance) * Time.fixedDeltaTime, 0, 0);
+            G_SettingsMenu.transform.position += new Vector3((G_Inversor*G_distance) * Time.fixedDeltaTime, 0, 0);
             if (G_Timer >= 0.5f) {
                 G_Timer = 0;
                 G_ChangeMenuAnimTrigger= false;
